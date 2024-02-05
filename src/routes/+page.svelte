d4aecff94f9868dbecc3da05ef6347d73f955e97
Francesca Pi <francecipinna@gmail.com>
2024-02-05 18:19:15 +0100

    does something!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,20 +24,37 @@
 			value: 1
 		}
 	];
+
+	let entries = [];
+
+	function addEntry(e) {
+		const formData = new FormData(e.target);
+		for(const [key, value] of formData) {
+			entries = [`${key}: ${value}`, ...entries];
+		}
+	}
 </script>
 
-<form>
+<form on:submit|preventDefault={addEntry}>
 	<fieldset>
 		<legend>How are you today?</legend>
 		{#each moods as mood}
 		<p>
-			<input type="radio" name="mood" id="mood-{mood.value}" value={mood.value} />
+			<input type="radio" name="mood" id="mood-{mood.value}" value={mood.name} />
 			<label for="mood-{mood.value}">{mood.name}</label>
 			<p>
 		{/each}
 	<button type="submit">Save</button>
 	</fieldset>
 </form>
+
+<div style="margin:10px; padding:2px; border: solid 1px black;">
+	{#each entries as entry}
+		<p class="diary-entry">
+			{entry}
+		</p>
+	{/each}
+</div>
 
 <style>
 	form {
