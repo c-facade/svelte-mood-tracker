702b5cdf78f3a717fd2dd17458fdf77ab87e76fa
c-facade <francecipinna@gmail.com>
2024-06-25 21:46:48 +0200

    persistent storage

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,13 @@
 import { app } from './firebaseConf';
 import { getAuth } from "firebase/auth";
-import { doc, getFirestore } from 'firebase/firestore';
+import { doc, initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore';
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+export const db = initializeFirestore(app, {
+    localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() })
+});
+
 // funzione che recupera l'ID del documento dell'utente
 export const userDoc = (userId: string) => doc(db, "users", userId)
 
