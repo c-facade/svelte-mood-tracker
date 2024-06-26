55951504b8465049df9830518a7efb84573c96ac
c-facade <francecipinna@gmail.com>
2024-06-26 11:34:52 +0200

    deploy

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,6 +8,15 @@ export const db = initializeFirestore(app, {
     localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() })
 });
 
+/*
+try{
+	enablePersistence(db)
+}
+catch(e){
+	console.log("Failed to enable persistence: " + e);
+	}
+ */
+
 // funzione che recupera l'ID del documento dell'utente
 export const userDoc = (userId: string) => doc(db, "users", userId)
 
