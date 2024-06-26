dcb7eb502d102098718039d98dab3a0513ca870d
c-facade <francecipinna@gmail.com>
2024-06-26 15:59:48 +0200

    removed vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		VitePWA({
-			devOptions: {
-				enabled: true
-			},
-			// cache all the imports
-			workbox: {
-				globPatterns: ["**/*"],
-			},
-			includeAssets: ["**/*"],
-			manifest: {
-				name: 'Mood Tracker',
-				short_name: 'MoodTracker',
-        description: 'Track your mood',
-				theme_color: '#009688',
-				background_color: 'black',
-        icons: [
-          {
-            src: 'favicon.svg',
-            sizes: '192x192',
-            type: 'image/svg'
-          },
-          {
-            src: 'favicon.svg',
-            sizes: '512x512',
-            type: 'image/svg'
-					},
-				]
-			}
-		})
-	],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
