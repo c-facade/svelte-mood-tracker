2b6e6ef2357170cc0a6e52a5524a81d8357cb9be
c-facade <francecipinna@gmail.com>
2024-06-26 17:30:16 +0200

    pwa

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { dev } from '$app/environment';
+
+if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/service-worker.js', { type: dev ? 'module' : 'classic'})
+	.then((registration) => {
+		console.log('Service Worker registered with scope:', registration.scope);
+	})
+	.catch((error) => {
+		console.error('Service Worker registration failed:', error);
+	});
+}
